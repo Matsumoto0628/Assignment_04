6f836423034d47d6eb64391d2fa0134ff5c5cc58
AtsuyuMatsumoto <108996238+Matsumoto0628@users.noreply.github.com>
2023-08-19 12:17:34 +0000

    [Finish]Bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,66 @@
+class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "create successfully"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:notice] = "create error"
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def index
+    @books = Book.all
+    @user = current_user
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "update successfully"
+      redirect_to book_path
+    else
+      flash.now[:notice] = "update error"
+      render :edit
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    user = User.find(book.user_id)
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+end
